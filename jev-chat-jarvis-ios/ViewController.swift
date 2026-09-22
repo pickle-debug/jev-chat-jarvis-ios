87a3d692c88a9e8749243752ea5e422627e6991a
gidon_ho <1941109171@qq.com>
2026-09-22 17:39:44 +0800

    Initial Commit

---
+++ jev-chat-jarvis-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jev-chat-jarvis-ios
+//
+//  Created by gidon on 2026/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
